185bed8559df2f160daf1e79f49f79afaeafd28a
berkantsahn <berkantsahn@gmail.com>
2023-06-03 13:47:02 +0300

    example of summing numbers

---
+++ klavyeden-girilen-iki-sayiyi-toplama.py
@@ -0,0 +1,5 @@
+print("Sıfırdan Yazılımcı - Python ile klavyeden girilen iki sayıyı toplayan program")
+sayi1 = int(input("1. Sayıyı giriniz: "))
+sayi2 = int(input("2. Sayıyı giriniz: "))
+toplam = sayi1 + sayi2
+print(sayi1,"+",sayi2," = ",toplam)
